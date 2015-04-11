92e6d0a288d049625af8af98e81cfb075518562f
NioZero <ghidalgo@frostdisk.com>
2015-04-11 10:58:33 -0300

    Uploading base project (missing code still, but compiling)

---
+++ CodeGen/src/CodeGen.App/FormMain.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace CodeGen.App
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.menuMain = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItemFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemProject = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemHelp = new System.Windows.Forms.ToolStripMenuItem();
+            this.panelMain = new System.Windows.Forms.Panel();
+            this.menuMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuMain
+            // 
+            this.menuMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItemFile,
+            this.toolStripMenuItemProject,
+            this.toolStripMenuItemHelp});
+            resources.ApplyResources(this.menuMain, "menuMain");
+            this.menuMain.Name = "menuMain";
+            // 
+            // toolStripMenuItemFile
+            // 
+            this.toolStripMenuItemFile.Name = "toolStripMenuItemFile";
+            resources.ApplyResources(this.toolStripMenuItemFile, "toolStripMenuItemFile");
+            // 
+            // toolStripMenuItemProject
+            // 
+            this.toolStripMenuItemProject.Name = "toolStripMenuItemProject";
+            resources.ApplyResources(this.toolStripMenuItemProject, "toolStripMenuItemProject");
+            // 
+            // toolStripMenuItemHelp
+            // 
+            this.toolStripMenuItemHelp.Name = "toolStripMenuItemHelp";
+            resources.ApplyResources(this.toolStripMenuItemHelp, "toolStripMenuItemHelp");
+            // 
+            // panelMain
+            // 
+            this.panelMain.BackColor = System.Drawing.SystemColors.Control;
+            resources.ApplyResources(this.panelMain, "panelMain");
+            this.panelMain.Name = "panelMain";
+            // 
+            // FormMain
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.Controls.Add(this.panelMain);
+            this.Controls.Add(this.menuMain);
+            this.Name = "FormMain";
+            this.menuMain.ResumeLayout(false);
+            this.menuMain.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuMain;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemFile;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemProject;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemHelp;
+        private System.Windows.Forms.Panel panelMain;
+
+    }
+}
+

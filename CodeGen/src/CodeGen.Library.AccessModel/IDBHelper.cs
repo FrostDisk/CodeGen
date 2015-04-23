1ea7416db9c97a477a545b2ca27b0c5bce2832ec
NioZero <ghidalgo@frostdisk.com>
2015-04-22 21:44:09 -0300

    Uploading some base library files

---
+++ CodeGen/src/CodeGen.Library.AccessModel/IDBHelper.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeGen.Library.AccessModel
+{
+    /// <summary>
+    /// DBHelper base interface
+    /// </summary>
+    public interface IDBHelper
+    {
+        string ConnectionString { get; }
+
+        string ConnectionStringKey { get; }
+
+        T GetEntity<T>(string storedProcedureName, ParameterCollection parameters, Func<DataRow, T> buildFunction) where T : new();
+
+        T GetEntity<T>(string storedProcedureName, ParameterCollection parameters, Func<IDataReader, T> buildFunction) where T : new();
+
+        T GetEntity<T>(string storedProcedureName, Parameter parameter, Func<DataRow, T> buildFunction) where T : new();
+
+        T GetEntity<T>(string storedProcedureName, Parameter parameter, Func<IDataReader, T> buildFunction) where T : new();
+
+        T GetEntity<T>(string storedProcedureName, Func<DataRow, T> buildFunction) where T : new();
+
+        T GetEntity<T>(string storedProcedureName, Func<IDataReader, T> buildFunction) where T : new();
+
+        T GetEntityFromSql<T>(string query, ParameterCollection parameters, Func<DataRow, T> buildFunction) where T : new();
+
+        T GetEntityFromSql<T>(string query, ParameterCollection parameters, Func<IDataReader, T> buildFunction) where T : new();
+
+        T GetEntityFromSql<T>(string query, Parameter parameter, Func<DataRow, T> buildFunction) where T : new();
+
+        T GetEntityFromSql<T>(string query, Parameter parameter, Func<IDataReader, T> buildFunction) where T : new();
+
+        T GetEntityFromSql<T>(string query, Func<DataRow, T> buildFunction) where T : new();
+
+        T GetEntityFromSql<T>(string query, Func<IDataReader, T> buildFunction) where T : new();
+
+        List<T> GetCollection<T>(string storedProcedureName, ParameterCollection parameters, Func<DataRow, T> buildFunction) where T : new();
+
+        List<T> GetCollection<T>(string storedProcedureName, ParameterCollection parameters, Func<IDataReader, T> buildFunction) where T : new();
+
+        List<T> GetCollection<T>(string storedProcedureName, Parameter parameter, Func<DataRow, T> buildFunction) where T : new();
+
+        List<T> GetCollection<T>(string storedProcedureName, Parameter parameter, Func<IDataReader, T> buildFunction) where T : new();
+
+        List<T> GetCollection<T>(string storedProcedureName, Func<DataRow, T> buildFunction) where T : new();
+
+        List<T> GetCollection<T>(string storedProcedureName, Func<IDataReader, T> buildFunction) where T : new();
+
+        List<T> GetCollectionFromSql<T>(string query, ParameterCollection parameters, Func<DataRow, T> buildFunction) where T : new();
+
+        List<T> GetCollectionFromSql<T>(string query, ParameterCollection parameters, Func<IDataReader, T> buildFunction) where T : new();
+
+        List<T> GetCollectionFromSql<T>(string query, Parameter parameter, Func<DataRow, T> buildFunction) where T : new();
+
+        List<T> GetCollectionFromSql<T>(string query, Parameter parameter, Func<IDataReader, T> buildFunction) where T : new();
+
+        List<T> GetCollectionFromSql<T>(string query, Func<DataRow, T> buildFunction) where T : new();
+
+        List<T> GetCollectionFromSql<T>(string query, Func<IDataReader, T> buildFunction) where T : new();
+
+        DataTable GetDataTable(string storedProcedureName, ParameterCollection parametros = null);
+
+        DataTable GetDataTable(string storedProcedureName, Parameter parameter);
+
+        DataTable GetDataTableFromSql(string query, ParameterCollection parameters = null);
+
+        DataTable GetDataTableFromSql(string query, Parameter parameter);
+
+        T GetScalar<T>(string storedProcedureName, ParameterCollection parameters = null);
+
+        T GetScalar<T>(string storedProcedureName, Parameter parameter);
+
+        T GetScalarFromSql<T>(string query, ParameterCollection parameters = null);
+
+        T GetScalarFromSql<T>(string query, Parameter parameter);
+
+        int ExecuteStoredProcedure(string storedProcedureName, ParameterCollection parameters = null);
+
+        int ExecuteStoredProcedure(string storedProcedureName, Parameter parameter);
+
+        int ExecuteSql(string query, ParameterCollection parameters = null);
+
+        int ExecuteSql(string query, Parameter parameter);
+    }
+}
---
+++ CodeGen/src/CodeGen.Library.System/Configuration/ConfigHelper.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Configuration;
+
+namespace CodeGen.Library.System.Configuration
+{
+    /// <summary>
+    /// Static class to access configuration files for application
+    /// </summary>
+    public static class ConfigHelper
+    {
+        /// <summary>
+        /// Get value from a application settings from a key
+        /// </summary>
+        /// <param name="key">unique key to identify the setting</param>
+        /// <param name="throwException">Throw exception if the value doesn't exist in the configuration file. If <c>false</c> this method will only return the default value</param>
+        /// <param name="defaultValue">Default value if the setting value doesn't exist in the configuration file.</param>
+        /// <returns></returns>
+        public static string GetValueWebConfig(string key, bool throwException = true, string defaultValue = "")
+        {
+            return GetValueAppConfig(key, throwException, defaultValue);
+        }
+
+        /// <summary>
+        /// Get value from a application settings from a key
+        /// </summary>
+        /// <param name="key">unique key to identify the setting</param>
+        /// <param name="throwException">Throw exception if the value doesn't exist in the configuration file. If <c>false</c> this method will only return the default value</param>
+        /// <param name="defaultValue">Default value if the setting value doesn't exist in the configuration file.</param>
+        /// <returns></returns>
+        public static string GetValueAppConfig(string key, bool throwException = true, string defaultValue = "")
+        {
+            string valor = ConfigurationManager.AppSettings.Get(key);
+
+            if (string.IsNullOrWhiteSpace(valor))
+            {
+                if (throwException)
+                {
+                    throw new NullReferenceException("The application configuration file doesn't have a value with the key '" + key + "'");
+                }
+                if (!string.IsNullOrWhiteSpace(defaultValue))
+                {
+                    ConfigurationManager.AppSettings.Set(key, defaultValue);
+                    return defaultValue;
+                }
+                return string.Empty;
+            }
+
+            return valor;
+        }
+
+        /// <summary>
+        /// Get the connection string from the configuration file
+        /// </summary>
+        /// <param name="connectionStringKey">Unique key to identify the connection string</param>
+        /// <returns></returns>
+        public static string GetConnectionString(string connectionStringKey)
+        {
+            ConnectionStringSettings connectionString = ConfigurationManager.ConnectionStrings[connectionStringKey];
+
+            if (connectionString == null || string.IsNullOrWhiteSpace(connectionString.ConnectionString))
+            {
+                throw new NullReferenceException("The application configuration file doesn't have a connection string with the key '" + connectionStringKey + "'");
+            }
+
+            return connectionString.ConnectionString;
+        }
+
+        /// <summary>
+        /// Refresh the values from the appSettings section in the configuration file
+        /// </summary>
+        public static void RefreshAppSettingsSection()
+        {
+            ConfigurationManager.RefreshSection("appSettings");
+        }
+
+        /// <summary>
+        /// Refresh the values from the connectionStrings section in the configuration file
+        /// </summary>
+        public static void RefreshConnectionStringsSection()
+        {
+            ConfigurationManager.RefreshSection("connectionStrings");
+        }
+    }
+}

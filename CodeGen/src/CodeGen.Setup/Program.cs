4d7b9c002d99fd5ef7b2e3223f37c3b73b4df4fb
NioZero <ghidalgo@frostdisk.com>
2015-08-12 16:52:22 -0300

    Adding some basic code in the setup project.

--- CodeGen/src/CodeGen.Setup/Program.cs
+++ CodeGen/src/CodeGen.Setup/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Windows.Forms;
 using WixSharp;
-using WixSharp.Forms;
 
 namespace WixSharpSetup
 {
@@ -9,57 +7,35 @@ namespace WixSharpSetup
     {
         static void Main()
         {
-            var project = new ManagedProject("MyProduct",
-                             new Dir(@"%ProgramFiles%\My Company\My Product",
-                                 new File("Program.cs")));
+            const string name = "CodeGen";
+            const string company = "FrostDisk Chile";
 
-            project.GUID = new Guid("6fe30b47-2577-43ad-9095-1861ba25889b");
+            var project = new Project()
+            {
+                Name = name,
+                GUID = new Guid("a48c5724-e8b9-4caf-a231-00f15057d15c"),
+                Dirs = new[]
+                    {
+                        new Dir(@"%ProgramFiles%\" + company + @"\" + name)
+                        {
+                            Files = new []
+                            {
+                                new File(@"CodeGen.exe"),
+                                new File(@"CodeGen.exe.config"),
+                                new File(@"license.txt"),
+                                new File(@"CodeGen.Data.dll"),
+                                new File(@"CodeGen.Domain.dll"),
+                                new File(@"CodeGen.Library.AccessModel.dll"),
+                                new File(@"CodeGen.Library.Formats.dll"),
+                                new File(@"CodeGen.Library.Security.dll"),
+                                new File(@"CodeGen.Library.System.dll"),
+                                new File(@"CodeGen.Plugin.Base.dll")
+                            }
+                        }
+                    }
+            };
 
-            // project.ManagedUI = ManagedUI.Empty;    //no standard UI dialogs
-            project.ManagedUI = ManagedUI.Default;  //all standard UI dialogs
-
-            //custom set of standard UI dialogs
-            project.ManagedUI = new ManagedUI();
-
-            project.ManagedUI.InstallDialogs.Add(Dialogs.Welcome)
-                                            .Add(Dialogs.Licence)
-                                            .Add(Dialogs.SetupType)
-                                            .Add(Dialogs.Features)
-                                            .Add(Dialogs.InstallDir)
-                                            .Add(Dialogs.Progress)
-                                            .Add(Dialogs.Exit);
-
-            project.ManagedUI.ModifyDialogs.Add(Dialogs.MaintenanceType)
-                                           .Add(Dialogs.Features)
-                                           .Add(Dialogs.Progress)
-                                           .Add(Dialogs.Exit);
-
-            project.Load += Msi_Load;
-            project.BeforeInstall += Msi_BeforeInstall;
-            project.AfterInstall += Msi_AfterInstall;
-
-            //project.SourceBaseDir = "<input dir path>";
-            //project.OutDir = "<output dir path>";
-
-            project.BuildMsi();
-        }
-
-        static void Msi_Load(SetupEventArgs e)
-        {
-            if (!e.IsUISupressed && !e.IsUninstalling)
-                MessageBox.Show(e.ToString(), "Load");
-        }
-
-        static void Msi_BeforeInstall(SetupEventArgs e)
-        {
-            if (!e.IsUISupressed && !e.IsUninstalling)
-                MessageBox.Show(e.ToString(), "BeforeInstall");
-        }
-
-        static void Msi_AfterInstall(SetupEventArgs e)
-        {
-            if (!e.IsUISupressed && !e.IsUninstalling)
-                MessageBox.Show(e.ToString(), "AfterExecute");
+            Compiler.BuildMsi(project);
         }
     }
 }

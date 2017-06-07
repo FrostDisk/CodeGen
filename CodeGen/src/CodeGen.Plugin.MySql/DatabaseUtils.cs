0ba1895880d69f7519b115cba26fbcbe80fe01db
NioZero <ghidalgo@frostdisk.com>
2017-06-07 12:08:47 -0400

    Replace MySql Reference by NuGet Package

--- CodeGen/src/CodeGen.Plugin.MySql/DatabaseUtils.cs
+++ CodeGen/src/CodeGen.Plugin.MySql/DatabaseUtils.cs
@@ -1,11 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
 using System.Collections.Generic;
-using System.Data;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MySql.Data.MySqlClient;
 
 namespace CodeGen.Plugin.MySql
 {

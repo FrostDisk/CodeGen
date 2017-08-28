0d01296ba1e8df8a4f4353b38913f6fa49d5be25
NioZero <ghidalgo@frostdisk.com>
2017-08-28 11:09:53 -0300

    Deleting MySql project because it was moved to another repo

--- CodeGen/src/CodeGen.Plugin.Base/Properties/AssemblyInfo.cs
+++ CodeGen/src/CodeGen.Plugin.Base/Properties/AssemblyInfo.cs
@@ -1,12 +1,11 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CodeGen.Plugin.Base")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("FrostDisk CodeGen API Plugin")]
 [assembly: AssemblyConfiguration("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
